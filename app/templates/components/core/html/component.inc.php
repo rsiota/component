7285bed2accaa5442a6cbe620a5a0b21645957d3
Ruben Siota Perez <rsiota.gmail.com>
2022-03-17 21:45:46 +0000

    Add animate mixin

--- app/templates/components/core/html/component.inc.php
+++ app/templates/components/core/html/component.inc.php
@@ -1,5 +1,7 @@
-<div class="container">
-	<button class="button">more info</button>
+<div class="container" x-data="{ clicked: true}">
+	<button class="button"
+		@click="clicked = !clicked"
+		:class="clicked && 'animation'">more info</button>
 	<div class="box" scroll></div>
 	<div class="second-box" scroll></div>
 </div>
