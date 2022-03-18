0ecfa024bc53dadc266c2ebc0c177c85ced6d7da
Ruben Siota Perez <rsiota.gmail.com>
2022-03-18 21:53:38 +0000

    Finish mixin refactor and fix bug when using different mixins together

--- app/templates/components/core/html/component.inc.php
+++ app/templates/components/core/html/component.inc.php
@@ -1,7 +1,10 @@
 <div class="container" x-data="{ clicked: true}">
 	<button class="button"
 		@click="clicked = !clicked"
-		:class="clicked && 'animation'">more info</button>
+		:class="clicked && 'animation'"
+		scroll>
+		more info
+	</button>
 	<div class="box" scroll></div>
 	<div class="second-box" scroll></div>
 </div>
